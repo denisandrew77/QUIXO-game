e302d26c46e3c6a59a73a9bee0008568085e936c
Denis <denisdamse@gmail.com>
2024-04-08 12:48:44 +0300

    created backround and table

---
+++ script.js
@@ -0,0 +1,16 @@
+function setup()
+{
+    createCanvas(700,700);
+}
+function draw()
+{
+    tablaDeJoc();
+}
+function tablaDeJoc()
+{
+    background("white");
+    fill(72,45,3);
+    ellipse(350,350,700,700);
+    fill(232, 189, 124);
+    rect(103,103,494,494);
+}
