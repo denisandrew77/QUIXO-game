e0b9eac9de5b110632ec89503cbb5b6bc0e8c7cf
Timotei Adam <timotei.adam@emanuel.ro>
2024-05-03 14:59:59 +0300

    adaugare buton joc nou

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let matrice = [];
 let dimensiuneBloc = 98.8;
 let coordonatePunctStart = 103;
+let buton;
 
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(900, 700);
+  buton = createButton('Joc Nou');
+  buton.position(750, 20);
+  buton.mousePressed(resetareJoc);
+  resetareJoc();
+}
+
+function resetareJoc() {
   for (i = 1; i <= 5; i++) {
     matrice[i] = [];
     for (j = 1; j <= 5; j++) {
@@ -16,19 +24,19 @@ function setup() {
     }
   }
 }
+
 function mousePressed() {
   for (i = 1; i <= 5; i++) {
     for (j = 1; j <= 5; j++) {
-      if ((i == 1 || i == 5 || j == 1 || j == 5) && mouseX >= matrice[i][j].coordonataX && mouseX <= matrice[i][j].coordonataX + dimensiuneBloc
-        && mouseY >= matrice[i][j].coordonataY && mouseY <= matrice[i][j].coordonataY + dimensiuneBloc) {
+      if ((i == 1 || i == 5 || j == 1 || j == 5) && mouseX >= matrice[i][j].coordonataX && mouseX <= matrice[i][j].coordonataX + dimensiuneBloc && mouseY >= matrice[i][j].coordonataY && mouseY <= matrice[i][j].coordonataY + dimensiuneBloc) {
         matrice[i][j].valoare = 1;
       }
     }
   }
-
-
 }
+
 function draw() {
+  butonJocNou();
   tablaDeJoc();
   for (i = 1; i <= 5; i++) {
     for (j = 1; j <= 5; j++) {
@@ -43,10 +51,20 @@ function draw() {
     }
   }
 }
+
 function tablaDeJoc() {
-  background("white");
+  background("#795548");
   fill(72, 45, 3);
   ellipse(350, 350, 700, 700);
-  fill(232, 189, 124);
   rect(103, 103, 494, 494);
+}
+
+function butonJocNou() {
+  buton.style('background-color', '#D6A797');
+  buton.style('color', 'black');
+  buton.style('font-size', '20px');
+  buton.style('padding', '10px');
+  buton.style('border', '5px');
+  buton.style('text-align', 'center');
+  buton.style('cursor', 'pointer');
 }
